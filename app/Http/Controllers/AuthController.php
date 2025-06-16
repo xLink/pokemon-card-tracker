bffec6139c66a082ec83c3e5cb3e95362f83a0f9
lisaohh <102254046+lisaohh@users.noreply.github.com>
2025-06-16 11:49:27 +0100

    checking if this goes up okay

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use \App\Models\User;
+use \Illuminate\Support\Str;
 
-class AuthController extends Controller 
+class AuthController extends Controller
 {
     public function getLogin() 
     {
@@ -42,6 +44,36 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
     
         return redirect('/');
+    }
+
+       public function getRegister() 
+    {
+        return inertia('Pages/RegisterPage');
+    }
+
+      public function postRegister(Request $request) 
+    {
+        //validate user inputs before adding to the database
+        $credentials = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // check if the user already exists
+        if (User::where('email', $request->input('email'))->exists()) {
+            return ['error' => true, 'message' => 'This email is already registered.'];
+        }
+
+        // create a new user and log them in
+        $user = new User();
+        $user->uuid = (string) Str::uuid();; 
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        Auth::login($user);
+        return inertia('Pages/Dashboard', ['error' => false, 'message' => 'You have successfully registered and logged in!']);
     }
 
 
